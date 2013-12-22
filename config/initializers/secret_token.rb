aeb504ee6378d378a0293d6a2025e320905ef762
Duncan Stuart <dgmstuart@gmail.com>
2013-12-22 19:32:41 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swingoutlondon2::Application.config.secret_key_base = 'db87180a263a5de8faceda7da1d45256157afa3ca650f814915343385ada77c099afa72c9e3a6070dc23ec2c4169c15ffdc9b3fab7c6d4fde0528177eed8a017'
