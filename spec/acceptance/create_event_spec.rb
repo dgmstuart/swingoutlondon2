e70312a6dbad799af930b335360847cbda8c06cc
Duncan Stuart <dgmstuart@gmail.com>
2014-08-03 23:41:27 +0100

    restructuring acceptance specs to be less dependent on the implementation

--- spec/acceptance/create_event_spec.rb
+++ spec/acceptance/create_event_spec.rb
@@ -1,48 +1,44 @@
 require 'rails_helper'
 
 def given_the_user_is_on_the_new_event_form
-  visit '/events/new'
+
 end
 
 feature "adding an event", type: :feature do
   let(:event) { Fabricate.build(:event) }
   scenario "User creates an event with a url" do
-    given_the_user_is_on_the_new_event_form
-    when_the_form_is_filled_in_with_valid_data
-    when_the_form_is_submitted
-    then_an_event_is_created
+    when_i_create_a_new_event_with_valid_data
+    then_the_event_should_be_displayed
   end
 
   scenario "User creates an event with invalid data" do
-    given_the_user_is_on_the_new_event_form
-    when_the_form_is_filled_in_with_invalid_data
-    when_the_form_is_submitted
+    when_i_create_an_event_with_invalid_data
     then_errors_should_be_displayed
   end
 
-  def when_the_form_is_filled_in_with_valid_data
+  def when_i_create_a_new_event_with_valid_data
+    visit '/events/new'
     within("#new_event") do
       fill_in 'event[url]', with: event.url
       select 'Intermittent', from: "event[frequency]"
       fill_in 'event[date]', with: event.date
-    end
-  end
 
-  def when_the_form_is_submitted
-    within("#new_event") do
       click_button 'Create event'
     end
   end
 
-  def then_an_event_is_created
+  def then_the_event_should_be_displayed
     expect(page).to have_content "New event created"
     expect(page).to have_content event.url
   end
 
-  def when_the_form_is_filled_in_with_invalid_data
+  def when_i_create_an_event_with_invalid_data
+    visit '/events/new'
     within("#new_event") do
       fill_in 'event[url]', with: "foo"
       fill_in 'event[date]', with: "3333/01/4/27"
+
+      click_button 'Create event'
     end
   end
 
@@ -59,13 +55,13 @@ feature "adding a weekly event", type: :feature do
   after { Timecop.return }
   let(:event) { Fabricate.build(:event) }
   scenario "User creates events for the next 4 weeks in the future" do
-    given_the_user_is_on_the_new_event_form
-    when_the_form_is_filled_in_with_a_weekly_repeating_event
-    then_events_should_be_created_for_the_next_4_weeks
-    then_events_for_the_next_4_weeks_are_displayed
+    when_i_create_a_weekly_repeating_event
+    then_events_for_the_next_4_weeks_should_be_displayed
+    and_events_for_the_next_4_weeks_should_be_displayed_in_the_events_list
   end
 
-  def when_the_form_is_filled_in_with_a_weekly_repeating_event
+  def when_i_create_a_weekly_repeating_event
+    visit '/events/new'
     within("#new_event") do
       fill_in 'event[url]', with: event.url
       select 'Weekly', from: "event[frequency]"
@@ -74,13 +70,13 @@ feature "adding a weekly event", type: :feature do
       click_button 'Create event'
     end
   end
-  def then_events_should_be_created_for_the_next_4_weeks
+  def then_events_for_the_next_4_weeks_should_be_displayed
     expect(page).to have_content "New event created"
     expect(page).to have_content "4 instances created"
     display_4_events
   end
 
-  def then_events_for_the_next_4_weeks_are_displayed
+  def and_events_for_the_next_4_weeks_should_be_displayed_in_the_events_list
     visit '/events'
     expect(page).to have_content(event.url, count: 4)
     display_4_events
