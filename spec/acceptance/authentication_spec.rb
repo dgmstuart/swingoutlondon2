69e35b86b6706d1b725b877ea4502cdf67954f73
Duncan Stuart <dgmstuart@gmail.com>
2014-08-14 19:06:18 +0100

    Authentication now has some halfway decent specs for failed login

--- spec/acceptance/authentication_spec.rb
+++ spec/acceptance/authentication_spec.rb
@@ -1,26 +1,27 @@
 require 'rails_helper'
 
 feature "Logged Out User tries to access a page:" do
-  before { Fabricate.create(:event, id: 1) }
-  before { Fabricate.create(:event_instance_with_dependencies) }
-  [
-    "event_instances"
-  ].each do |page|
-    scenario page do
-      visit page
-      raise "NOT IMPLEMENTED"
+  before { @event_instance = Fabricate.create(:event_instance_with_dependencies) }
+  let(:event) { @event_instance.event_seed.event }
+  {
+    "event_instances" => "Event Instances",
+  }.each do |path, text|
+    scenario path do
+      visit path
+      expect(page).to have_content(text)
     end
   end
 
-  [
-    "/events/1/dates/new",
-    "/events",
-    "/events/new",
-    "/events/1",
-  ].each do |page|
-    scenario page do
-      visit page
-      raise "NOT IMPLEMENTED"
+  # TODO - find a better way to do this
+  {
+    "new_event_date" => ->(id) { "/events/#{id}/dates/new" },
+    "list_events"    => ->(id) { "/events" },
+    "new_event"      => ->(id) { "/events/new" },
+    "show_event"     => ->(id) { "/events/#{id}" },
+  }.each do |name, path|
+    scenario name do
+      visit path.call(event.to_param)
+      expect(page).to have_content('Sign in')
     end
   end
   # TODO: The above are just GET - Also prevent PUT etc
