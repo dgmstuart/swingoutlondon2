49d5aaa887a765795aeedb0557f13c52acf164c2
Duncan Stuart <dgmstuart@gmail.com>
2014-08-14 23:58:07 +0100

    Adding acceptance specs for logging in

--- spec/acceptance/authentication_spec.rb
+++ spec/acceptance/authentication_spec.rb
@@ -26,3 +26,44 @@ feature "Logged Out User tries to access a page:" do
   end
   # TODO: The above are just GET - Also prevent PUT etc
 end
+
+feature "Logged Out User logs in" do
+  let(:user) { Fabricate.create(:user) }
+  scenario "with valid details" do
+    when_i_sign_in_with_valid_details
+    then_i_should_not_be_logged_in
+  end
+
+  scenario "with an invalid password" do
+    when_i_sign_in_with_an_invalid_password
+    then_i_should_be_logged_in
+  end
+
+  def when_i_sign_in_with_valid_details
+    sign_in_with(user.email, user.password)
+  end
+
+  def when_i_sign_in_with_an_invalid_password
+    sign_in_with(user.email, "nonsense")
+  end
+
+  def when_i_sign_in_with_an_invalid_email_address
+    sign_in_with("foo@bar.com", user.password)
+  end
+
+  def then_i_should_not_be_logged_in
+    expect(page).to have_content('Sign in')
+  end
+
+  def then_i_should_be_logged_in
+    expect(page).to have_content('Sign out')
+  end
+
+  def sign_in_with(email, password)
+    visit sign_in_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Sign up'
+  end
+end
+
---
+++ spec/fabricators/users.rb
@@ -0,0 +1,3 @@
+Fabricator(:user) do
+  email "me@mine.com"
+end
