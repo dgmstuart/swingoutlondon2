e85d34e80c08de0cb1b6385f52f6094514b4d2de
Duncan Stuart <dgmstuart@gmail.com>
2014-08-15 14:37:30 +0100

    Trying to get devise secret key to be recognised on Heroku - the issue is that the rails environment isn't available when deploying

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,12 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  config.secret_key = Rails.application.secrets.devise_secret_key
+  config.secret_key =
+    if Rails.env.production?
+      ENV["DEVISE_SECRET_KEY"]
+    else
+      Rails.application.secrets.devise_secret_key
+    end
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
