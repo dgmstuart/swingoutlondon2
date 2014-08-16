b3f594f8b06cac4a5acd7da213cd4c411d57483c
Duncan Stuart <dgmstuart@gmail.com>
2014-08-16 14:36:06 +0100

    Add Date form now displays validation failures

--- spec/acceptance/add_dates_spec.rb
+++ spec/acceptance/add_dates_spec.rb
@@ -12,7 +12,11 @@ feature "Admin adds dates to an event:" do
     then_those_dates_should_display_on_the_events_page
   end
 
-  scenario "User tries to add an invalid date"
+  scenario "an invalid date" do
+    given_an_event
+    when_i_add_an_invalid_date_to_the_event
+    then_errors_should_be_displayed
+  end
 
   def given_an_event
     event
@@ -34,5 +38,20 @@ feature "Admin adds dates to an event:" do
   def then_those_dates_should_display_on_the_events_page
     expect(page).to have_content "2014-12-23"
     expect(page).to have_content "2014-12-24"
+  end
+
+  def when_i_add_an_invalid_date_to_the_event
+    visit "/events"
+    click_link event.name
+
+    click_link "add date"
+    fill_in "event_generator[start_date]", with: "12/23/2014"
+    click_button "done"
+  end
+
+  def then_errors_should_be_displayed
+    expect(page).to have_content "errors prevented this event from being saved"
+    expect(page).to have_content "is not a date"
+    expect(page).to have_content "can't be blank" # Should be the date field
   end
 end
