70b203d11e17bf580ee677214d069c1028cd4219
Duncan Stuart <dgmstuart@gmail.com>
2016-01-21 16:17:01 +0000

    (spec): remove filter :focus
    It clutters up the beginning of the spec and I don't use it now that I'm
    mostly running specific specs through Vim

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,6 @@ end
 require 'rspec/core' # Zeus won't work without this because otherwise something else (?) is defining Rspec (?) and doesn't have access to the configure method (???)
 
 RSpec.configure do |config|
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
-  config.alias_example_to :ffeature, focus: true
-  config.alias_example_to :fscenario, focus: true
-
   if config.files_to_run.one?
     # Use the documentation formatter for detailed output
     config.default_formatter = 'doc'
