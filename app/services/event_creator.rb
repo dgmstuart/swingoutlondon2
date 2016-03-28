5e2441bffb53e2763341dfe65705b00834ae0a43
Duncan Stuart <dgmstuart@gmail.com>
2016-03-28 01:37:48 +0100

    (refactor): EventCreator creates seed & instance
    Only a single instance at this point. We'll deal with generating
    multiple instances in a bit.

--- app/services/event_creator.rb
+++ app/services/event_creator.rb
@@ -2,6 +2,13 @@ class EventCreator
   def call(event_form, create_venue)
     if event_form.valid?
       event = Event.create!(name: event_form.name)
+      event_seed = EventSeed.create! \
+        event: event,
+        url: event_form.url,
+        venue_id: event_form.venue_id
+      EventInstance.create! \
+        event_seed: event_seed,
+        date: event_form.start_date
       Success.new(event)
     else
       Failure.new
--- spec/services/event_creator_spec.rb
+++ spec/services/event_creator_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 require 'app/services/event_creator'
 
 RSpec.describe EventCreator do
-  context "when the event was successfully created" do
+  context "when the event was intermittent and successfully created" do
+    before { stub_models }
+
     it "creates an event with the given name" do
       event_klass = fake_event_klass
       stub_const("Event", event_klass)
@@ -13,16 +15,41 @@ RSpec.describe EventCreator do
       expect(event_klass).to have_received(:create!).with(name: "The Razmatazz club")
     end
 
+    it "creates an event seed with the given url and venue_id" do
+      event_seed_klass = fake_event_seed_klass
+      stub_const("EventSeed", event_seed_klass)
+      form = fake_event_form \
+        url: "http://consider.com/phlebas",
+        venue_id: 17
+
+      described_class.new.call(form, anything)
+
+      expect(event_seed_klass).to have_received(:create!)
+        .with \
+          url: "http://consider.com/phlebas",
+          event: anything, # too complicated to test the specific value?
+          venue_id: 17
+    end
+
+    it "creates an event instance with the given date" do
+      event_instance_klass = fake_event_instance_klass
+      stub_const("EventInstance", event_instance_klass)
+      form = fake_event_form(start_date: Date.new(2012, 12, 23))
+
+      described_class.new.call(form, anything)
+
+      expect(event_instance_klass).to have_received(:create!)
+        .with \
+          event_seed: anything, # too complicated to test the specific value?
+          date: Date.new(2012, 12, 23)
+    end
+
     describe "result" do
       it "is successful" do
-        stub_event_klass
-
         expect(described_class.new.call(fake_event_form, anything).success?).to eq true
       end
 
       it "returns a success message" do
-        stub_event_klass
-
         expect(described_class.new.call(fake_event_form, anything).message).to eq "New event created"
       end
 
@@ -40,24 +67,48 @@ RSpec.describe EventCreator do
   context "when there are validation errors" do
     describe "result" do
       it "is a failure" do
-        stub_const("Event", double)
         fake_invalid_event_form = instance_double("EventForm", valid?: false)
         expect(described_class.new.call(fake_invalid_event_form, anything).success?).to eq false
       end
     end
   end
 
-  def fake_event_form(name: "The Black Cotton Club")
-    instance_double("EventForm", name: name, valid?: true)
+  def fake_event_form(
+    name: "The Black Cotton Club",
+    start_date: Date.today,
+    url: "https://google.com",
+    venue_id: 34
+  )
+    instance_double("EventForm",
+                    name: name,
+                    start_date: start_date,
+                    url: url,
+                    venue_id: venue_id,
+                    valid?: true,
+                   )
   end
 
-  def stub_event_klass
+  def stub_models
     stub_const("Event", fake_event_klass)
+    stub_const("EventSeed", fake_event_seed_klass)
+    stub_const("EventInstance", fake_event_instance_klass)
   end
 
   def fake_event_klass(created_event: nil)
     class_double("Event").tap do |event_klass|
       allow(event_klass).to receive(:create!)
+    end
+  end
+
+  def fake_event_seed_klass
+    class_double("EventSeed").tap do |event_seed_klass|
+      allow(event_seed_klass).to receive(:create!)
+    end
+  end
+
+  def fake_event_instance_klass
+    class_double("EventInstance").tap do |event_instance_klass|
+      allow(event_instance_klass).to receive(:create!)
     end
   end
 end
