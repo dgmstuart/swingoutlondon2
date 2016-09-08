6493ceccbd42356cb378ae577ff4b95fdb07729d
Duncan Stuart <dgmstuart@gmail.com>
2016-09-09 00:34:00 +0100

    (fix): EventInstance dates aren't restricted
    Event instances had validations preventing them from being created too
    far in the past or the future - this is intended to prevent obvious
    mistakes.
    
    This causes problems when importing events from the old database: there
    are dates further in the past than this range allows, so the import
    failed.
    
    The point at which we actually care about setting these dates is when
    creating a new event. There are validations on the event form object
    which implement the same date range, so this restriction can be removed
    from the model.

--- app/models/event_instance.rb
+++ app/models/event_instance.rb
@@ -4,22 +4,19 @@ class EventInstance < ActiveRecord::Base
   belongs_to :venue, inverse_of: :event_instances
 
   validates :event_seed, presence: true
-  validates :date, presence: true, date: {
-    after: Proc.new { Date.today- 6.months },
-    before: Proc.new { Date.today + 1.year },
-    allow_blank: true
-  }
+  validates :date, presence: true, date: { allow_blank: true }
 
   default_scope -> { order(date: :asc) }
 
   def name
     event.name
   end
+
   def url
     @url || event_seed.url
   end
+
   def venue
     @venue || event_seed.venue
   end
-
 end
