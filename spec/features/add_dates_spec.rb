e79146b229818ef4c1db4c0b2bb4667dde16ee2d
Duncan Stuart <dgmstuart@gmail.com>
2017-05-01 00:50:31 +0100

    (refactor spec) inline helper functions
    I've decided to move away from the other helper function approach for
    actual steps: it makes the test more involved to read, but ultimately
    easier to understand and change

--- spec/features/add_dates_spec.rb
+++ spec/features/add_dates_spec.rb
@@ -6,60 +6,45 @@ RSpec.feature 'Admin adds dates to an event:' do
     login_as(user, scope: :user)
   end
 
-  let(:event) do
-    Fabricate.create(:event) do
-      event_seeds(count: 1)
-    end
-  end
   scenario 'two non-repeating dates' do
-    Timecop.freeze(Date.new(2001, 1, 20)) do
-      given_an_event
-      when_i_add_two_dates_to_the_event
-      then_those_dates_should_display_on_the_events_page
-    end
-  end
+    event = non_repeating_event
 
-  scenario 'an invalid date' do
-    given_an_event
-    when_i_add_an_invalid_date_to_the_event
-    then_errors_should_be_displayed
-  end
-
-  def given_an_event
-    event
-  end
-
-  def when_i_add_two_dates_to_the_event
     visit '/events'
 
     click_link event.name
 
-    click_link 'Add date'
-    fill_in 'event_instance[date]', with: '22/01/2001'
-    click_button 'Done'
+    Timecop.freeze(Date.new(2001, 1, 20)) do
+      click_link 'Add date'
+      fill_in 'event_instance[date]', with: '22/01/2001'
+      click_button 'Done'
 
-    click_link 'Add date'
-    fill_in 'event_instance[date]', with: '23/01/2001'
-    click_button 'Done'
-  end
+      click_link 'Add date'
+      fill_in 'event_instance[date]', with: '23/01/2001'
+      click_button 'Done'
+    end
 
-  def then_those_dates_should_display_on_the_events_page
     expect(page).to have_content '22/01/2001'
     expect(page).to have_content '23/01/2001'
   end
 
-  def when_i_add_an_invalid_date_to_the_event
+  scenario 'an invalid date' do
+    event = non_repeating_event
+
     visit '/events'
     click_link event.name
 
     click_link 'Add date'
     fill_in 'event_instance[date]', with: '12/23/2014'
     click_button 'Done'
-  end
 
-  def then_errors_should_be_displayed
     expect(page).to have_content 'errors prevented this event from being saved'
     expect(page).to have_content 'is not a date'
     expect(page).to have_content "can't be blank" # Should be the date field
+  end
+
+  def non_repeating_event
+    Fabricate.create(:event) do
+      event_seeds(count: 1)
+    end
   end
 end
