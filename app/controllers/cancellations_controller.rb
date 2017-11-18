32060078bb7b3ea68b4ff5d5a8d01e9264e9f5b0
Duncan Stuart <dgmstuart@gmail.com>
2017-11-18 17:51:42 +0000

    (chore) allow use of update_attribute
    I still think this is appropriate in these specific cases

--- app/controllers/cancellations_controller.rb
+++ app/controllers/cancellations_controller.rb
@@ -8,7 +8,7 @@ class CancellationsController < ApplicationController
 
     # update_attribute skips validations, but that's probably what we want here,
     # since we want to mark it as cancelled, even if (for some reason) it was already invalid
-    event_instance.update_attribute(:cancelled, true)
+    event_instance.update_attribute(:cancelled, true) # rubocop:disable Rails/SkipsModelValidations
 
     redirect_back(fallback_location: root_path)
   end
@@ -20,7 +20,7 @@ class CancellationsController < ApplicationController
 
     # update_attribute skips validations, but that's probably what we want here,
     # since we want to mark it as cancelled, even if (for some reason) it was already invalid
-    event_instance.update_attribute(:cancelled, false)
+    event_instance.update_attribute(:cancelled, false) # rubocop:disable Rails/SkipsModelValidations
 
     redirect_back(fallback_location: root_path)
   end
